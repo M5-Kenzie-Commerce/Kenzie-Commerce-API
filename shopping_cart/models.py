aad90503aadeecbbb5047851dad47704bfd41490
Flipsy1 <felipevie@hotmail.com>
2023-03-09 11:29:54 -0400

    feat/card_product-0.3

--- shopping_cart/models.py
+++ shopping_cart/models.py
@@ -2,14 +2,25 @@ from django.db import models
 import uuid
 
 
+class Cart(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    products = models.ManyToManyField(
+        "",
+        through="shopping_cart.CartProduct",
+        related_name="carts",
+    )
+
+
 class CartProduct(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     cart = models.ForeignKey(
-        "users.Cart", on_delete=models.CASCADE, related_name="cart_products"
+        "shopping_cart.Cart",
+        on_delete=models.CASCADE,
+        related_name="cart_cart_products",
     )
 
     product = models.ForeignKey(
-        "products.Product",
+        "",
         on_delete=models.CASCADE,
         related_name="product_cart_products",
     )
