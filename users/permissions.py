bab33e2509c70c8365a6e0f687fd6aaba8c0b6fc
Victoria <victoria-milan@outlook.com>
2023-03-12 20:44:33 -0300

    fix: fix user permission

--- users/permissions.py
+++ users/permissions.py
@@ -7,7 +7,7 @@ class UserPermission(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return (
             request.method in permissions.SAFE_METHODS
-            or request.user.is_authenticated
+            and request.user.is_authenticated
             or request.user.is_superuser
             or request.user == obj
         )
